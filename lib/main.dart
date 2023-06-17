4fcb7d7ee7f8a928d185564eae3d5207e14ec8be
hshehabu <hamzashehabu457@gmail.com>
2023-06-17 15:47:35 +0300

    first message

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: RootPage(),
+
+    );
+  }
+}
+class RootPage extends StatefulWidget {
+  const RootPage({Key? key}) : super(key: key);
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Flutter'
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
