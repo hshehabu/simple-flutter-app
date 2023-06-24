9c58bac4ac2f55c9664f94a5dcd2100b6a3bdce6
hshehabu <hamzashehabu457@gmail.com>
2023-06-24 14:22:12 +0300

    scrollable view

--- lib/happy_coding.dart
+++ lib/happy_coding.dart
@@ -8,6 +8,9 @@ class HappyCoding extends StatefulWidget {
 }
 
 class _HappyCodingState extends State<HappyCoding> {
+  bool isSwitch = false;
+
+  bool? isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,58 +18,136 @@ class _HappyCodingState extends State<HappyCoding> {
         title: const Text('happy coding'),
         automaticallyImplyLeading: false,
         leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        actions: [
+          IconButton(
             onPressed: () {
-              Navigator.of(context).pop();
+              debugPrint('information');
             },
-            icon: const Icon(Icons.arrow_back_ios)),
+            icon: const Icon(Icons.info_outline),
+          ),
+        ],
       ),
-      body: Column(
-        children: [
-          Center(
-              child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/download.jfif'),
-          )),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(),
-          const Center(
-            child: Text(
-              'Happy coding message from jimma university',
-              style: TextStyle(color: Colors.black54),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Icon(Icons.star),
-              Icon(Icons.star),
-              Icon(Icons.star),
-              Icon(Icons.star),
-              Icon(Icons.star),
-            ],
-          ),
-       const SizedBox(
-         height: 20,
-       ),
-          SizedBox(
-            height: 125,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Center(
+              child: Image.asset('images/download.jfif'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(),
+            const Center(
+              child: Text(
+                'Happy coding message from jimma university',
+                style: TextStyle(color: Colors.black54),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(onPressed: (){}, child: const Text('LIKE')),
-                OutlinedButton(onPressed: (){}, child: const Text('SHARE')),
-                TextButton(onPressed: (){}, child: const Text('COMMENT'))
-
+                Icon(Icons.star, color: isSwitch ? Colors.yellow : null),
+                Icon(Icons.star, color: isSwitch ? Colors.yellow : null),
+                Icon(Icons.star, color: isSwitch ? Colors.yellow : null),
+                Icon(Icons.star, color: isSwitch ? Colors.yellow : null),
+                Icon(Icons.star, color: isSwitch ? Colors.yellow : null),
               ],
             ),
-          ),
-
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 125,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton.icon(
+                      onPressed: () {},
+                      label: const Text('LIKE'),
+                      icon: const Icon(
+                        Icons.thumb_up,
+                        size: 15,
+                      )),
+                  OutlinedButton.icon(
+                      onPressed: () {},
+                      label: const Text('SHARE'),
+                      icon: const Icon(
+                        Icons.share,
+                        size: 15,
+                      )),
+                  TextButton.icon(
+                    onPressed: () {},
+                    label: const Text('COMMENT'),
+                    icon: const Icon(
+                      Icons.comment,
+                      size: 15,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint('a gesture detected');
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                  Text('FIRE'),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  )
+                ],
+              ),
+            ),
+            Switch(
+                value: isSwitch,
+                onChanged: (bool newBool) {
+                  setState(() {
+                    isSwitch = newBool;
+                  });
+                }),
+            Checkbox(
+                value: isChecked,
+                onChanged: (bool? newBool) {
+                  setState(() {
+                    isChecked = newBool;
+                  });
+                }),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(),
+            const SizedBox(
+              height: 30,
+            ),
+            Image.network(
+                'https://ju.edu.et/jit/wp-content/uploads/sites/11/2021/12/1-1-d-1024x416.png'),
+            const SizedBox(
+              height: 30,
+            ),
+          ],
+        ),
       ),
     );
   }
