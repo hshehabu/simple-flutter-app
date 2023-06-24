0540e550b483ebc290d3d257467d33b4709252d1
hshehabu <hamzashehabu457@gmail.com>
2023-06-24 13:40:47 +0300

    engagement buttons

--- lib/happy_coding.dart
+++ lib/happy_coding.dart
@@ -10,21 +10,64 @@ class HappyCoding extends StatefulWidget {
 class _HappyCodingState extends State<HappyCoding> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-
+    return Scaffold(
       appBar: AppBar(
         title: const Text('happy coding'),
         automaticallyImplyLeading: false,
-        leading: IconButton(onPressed: (){
-          Navigator.of(context).pop();
-        }, icon: const Icon(Icons.arrow_back_ios)),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(Icons.arrow_back_ios)),
       ),
-body: Column(
-  children: [
-    Image.asset('images/download.jfif'),
-  ],
-),
+      body: Column(
+        children: [
+          Center(
+              child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/download.jfif'),
+          )),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(),
+          const Center(
+            child: Text(
+              'Happy coding message from jimma university',
+              style: TextStyle(color: Colors.black54),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(Icons.star),
+              Icon(Icons.star),
+              Icon(Icons.star),
+              Icon(Icons.star),
+              Icon(Icons.star),
+            ],
+          ),
+       const SizedBox(
+         height: 20,
+       ),
+          SizedBox(
+            height: 125,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(onPressed: (){}, child: const Text('LIKE')),
+                OutlinedButton(onPressed: (){}, child: const Text('SHARE')),
+                TextButton(onPressed: (){}, child: const Text('COMMENT'))
+
+              ],
+            ),
+          ),
+
+        ],
+      ),
     );
   }
 }
-
